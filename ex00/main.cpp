22dc8cb5a608cc8a5a6230bfd356dcc55625cd3b
ysoroko <ysoroko@student.s19.be>
2021-10-14 16:39:33 +0200

    name conflict solved

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,50 +6,13 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 14:59:21 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/10/14 16:18:20 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/10/14 16:38:48 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "whatever.hpp"
-using std::cout;
-using std::endl;
 
-// -------------------------- HELPER FUNCTIONS ------------------------------
-template< typename T>
-static void	ft_print_pair(T a, T b)
-{
-	std::cout << a << " | " <<  b << std::endl;
-}
-
-template< typename T>
-static void ft_test(T a, T b, std::string type)
-{
-
-	std::cout << type << std::endl;
-	std::cout << "----------------------------" << std::endl;
-	
-	// Swap test
-	std::cout << "SWAP: " << std::endl;
-	std::cout << "Before swapping: \n";
-	ft_print_pair(a, b);
-	std::cout << "------" << std::endl;
-	swap(a, b);
-	std::cout << "After swapping: \n";
-	ft_print_pair(a, b);
-	std::cout << std::endl;
-
-	// Min test
-	std::cout << "MIN: " << std::endl;
-	std::cout << mmin(a, b) << std::endl << std::endl;
-
-	// Max test	
-	std::cout << "MAX: " << std::endl;
-	std::cout << mmax(a, b) << std::endl << std::endl;
-	std::cout << std::endl << std::endl;
-}
-
-// ------------------------------- MAIN -------------------------------------
 int	main( void )
 {
 	// INT
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 14:55:47 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/10/14 16:16:11 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/10/14 16:38:39 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,47 @@ void swap( T & x, T & y){
 }
 
 template< typename T >
-T const &	mmin( T const & x, T const & y) {
+T const &	min( T const & x, T const & y) {
 	return (x<y ? x : y);
 }
 
 template< typename T >
-T const &	mmax( T const & x, T const & y) {
+T const &	max( T const & x, T const & y) {
 	return (x>y ? x : y);
+}
+
+// -------------------------- HELPER FUNCTIONS ------------------------------
+template< typename T>
+void	ft_print_pair(T a, T b)
+{
+	std::cout << a << " | " <<  b << std::endl;
+}
+
+template< typename T>
+void ft_test(T a, T b, std::string type)
+{
+
+	std::cout << type << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	
+	// Swap test
+	std::cout << "SWAP: " << std::endl;
+	std::cout << "Before swapping: \n";
+	ft_print_pair(a, b);
+	std::cout << "------" << std::endl;
+	swap(a, b);
+	std::cout << "After swapping: \n";
+	ft_print_pair(a, b);
+	std::cout << std::endl;
+
+	// Min test
+	std::cout << "MIN: " << std::endl;
+	std::cout << ::min(a, b) << std::endl << std::endl;
+
+	// Max test	
+	std::cout << "MAX: " << std::endl;
+	std::cout << ::max(a, b) << std::endl << std::endl;
+	std::cout << std::endl << std::endl;
 }
 
 #endif
