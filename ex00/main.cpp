fb4a6c3d0c411da69837840b185cdb8728b4630c
ysoroko <ysoroko@student.s19.be>
2021-10-14 16:42:07 +0200

    subject main added

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 14:59:21 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/10/14 16:38:48 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/10/14 16:41:47 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,3 +54,20 @@ int	main( void )
 
 	return (0);
 }
+
+// ---------------------------- SUBJECT MAIN -----------------------------------
+// int main( void ) {
+// 	int a = 2;
+// 	int b = 3;
+// 	::swap( a, b );
+// 	std::cout << "a = " << a << ", b = " << b << std::endl;
+// 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+// 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+// 	std::string c = "chaine1";
+// 	std::string d = "chaine2";
+// 	::swap(c, d);
+// 	std::cout << "c = " << c << ", d = " << d << std::endl;
+// 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+// 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+// 	return 0;
+// }
